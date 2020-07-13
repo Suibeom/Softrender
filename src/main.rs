2ff8e14de29eb53dc7085e87847905b02d8b326c
Suibeom <suibeom@gmail.com>
2020-07-12 21:42:14 -0600

    boom!!

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use sdl2::pixels::PixelFormatEnum;
 
 use rand::Rng;
 
+use std::ops;
 struct Triangle<T, C> {
     pt1: T,
     pt2: T,
@@ -24,17 +25,76 @@ impl RGBA {
     }
 }
 
-struct Pt3 {
-    x: u32,
-    y: u32,
-    z: u32,
+#[derive(Copy, Clone)]
+struct Vertex<T, U> {
+    spatial: T,
+    uv: U,
 }
 
+#[derive(Copy, Clone)]
+struct Pt3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+struct ViewportData {
+    plane_basis_x: Pt3,
+    plane_basis_y: Pt3,
+    x_min: f64,
+    x_max: f64,
+    y_min: f64,
+    y_max: f64,
+    pixels_tall: usize,
+    pixels_wide: usize,
+}
+
+impl ops::Add<Pt3> for Pt3 {
+    type Output = Pt3;
+    fn add(self, rhs: Pt3) -> Pt3 {
+        return Pt3 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        };
+    }
+}
+
+impl ops::Sub<Pt3> for Pt3 {
+    type Output = Pt3;
+    fn sub(self, rhs: Pt3) -> Pt3 {
+        return Pt3 {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        };
+    }
+}
+
+impl ops::Mul<Pt3> for Pt3 {
+    type Output = f64;
+    fn mul(self, rhs: Pt3) -> f64 {
+        return self.x * rhs.x + self.y * rhs.y + self.z * rhs.z;
+    }
+}
+
+impl ops::Mul<f64> for Pt3 {
+    type Output = Pt3;
+    fn mul(self, rhs: f64) -> Pt3 {
+        return Pt3 {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        };
+    }
+}
 #[derive(Copy, Clone)]
 struct Pt2 {
     x: usize,
     y: usize,
+    clipped: bool,
 }
+
 fn get_color() -> RGBA {
     let color = rand::thread_rng().gen::<u32>();
     let color = color & 0xFFFFFF;
@@ -42,31 +102,39 @@ fn get_color() -> RGBA {
 }
 
 fn make_triangle_partition(
-    pixels_width: usize,
-    pixels_height: usize,
+    x_min: f64,
+    x_max: f64,
+    y_min: f64,
+    y_max: f64,
     grid_width: usize,
     grid_height: usize,
-    jitter: usize,
-) -> Vec<Triangle<Pt2, RGBA>> {
-    let float_pitch_x = pixels_width as f32 / grid_width as f32;
-    let float_pitch_y = pixels_height as f32 / grid_height as f32;
-    let mut points = vec![Pt2 { x: 0, y: 0 }; (grid_height + 1) * (grid_width + 1)];
+    jitter: f64,
+) -> Vec<Triangle<Vertex<Pt3, ()>, RGBA>> {
+    let float_pitch_x = (x_max - x_min) / grid_width as f64;
+    let float_pitch_y = (y_max - y_min) / grid_height as f64;
+    let mut points = vec![
+        Pt3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0
+        };
+        (grid_height + 1) * (grid_width + 1)
+    ];
     for i in 0..grid_height {
         for j in 0..grid_width {
             let (down, right): (bool, bool) = (rand::random(), rand::random());
-            points[i + grid_height * j] = Pt2 {
-                x: match down {
-                    false => (i as f32 * float_pitch_x) as usize,
-                    true => (i as f32 * float_pitch_x) as usize + jitter,
-                },
-                y: match right {
-                    false => (j as f32 * float_pitch_y) as usize,
-                    true => (j as f32 * float_pitch_y) as usize + jitter,
-                },
+            let x = match down {
+                false => (i as f64 * float_pitch_x) + x_min,
+                true => (i as f64 * float_pitch_x) + x_min + jitter,
             };
+            let y = match right {
+                false => (j as f64 * float_pitch_y) + y_min,
+                true => (j as f64 * float_pitch_y) + y_min + jitter,
+            };
+            points[i + grid_height * j] = Pt3 { x, y, z: 1.0 };
         }
     }
-    let mut triangles: Vec<Triangle<Pt2, RGBA>> = Vec::new();
+    let mut triangles: Vec<Triangle<Vertex<Pt3, _>, RGBA>> = Vec::new();
     let ul_grid_height = (grid_height as i64 - 1) as usize;
     let ul_grid_width = (grid_width as i64 - 1) as usize;
 
@@ -76,33 +144,69 @@ fn make_triangle_partition(
                 // true => slice from i,j to i+1, j+1
                 // false => slice from i, j+1 to i+1, j
                 true => {
-                    triangles.push(normal_form(Triangle {
-                        pt1: points[i + grid_height * j],
-                        pt2: points[i + 1 + grid_height * j],
-                        pt3: points[(i + 1) + grid_height * (j + 1)],
+                    triangles.push(Triangle {
+                        pt1: Vertex {
+                            spatial: points[i + grid_height * j],
+                            uv: (),
+                        },
+                        pt2: Vertex {
+                            spatial: points[i + 1 + grid_height * j],
+                            uv: (),
+                        },
+                        pt3: Vertex {
+                            spatial: points[(i + 1) + grid_height * (j + 1)],
+                            uv: (),
+                        },
                         color: get_color(),
-                    }));
-                    triangles.push(normal_form(Triangle {
-                        pt1: points[i + grid_height * j],
-                        pt2: points[i + grid_height * (j + 1)],
-                        pt3: points[i + 1 + grid_height * (j + 1)],
+                    });
+                    triangles.push(Triangle {
+                        pt1: Vertex {
+                            spatial: points[i + grid_height * j],
+                            uv: (),
+                        },
+                        pt2: Vertex {
+                            spatial: points[i + grid_height * (j + 1)],
+                            uv: (),
+                        },
+                        pt3: Vertex {
+                            spatial: points[i + 1 + grid_height * (j + 1)],
+                            uv: (),
+                        },
                         color: get_color(),
-                    }));
+                    });
                 }
 
                 false => {
-                    triangles.push(normal_form(Triangle {
-                        pt1: points[i + grid_height * j],
-                        pt2: points[i + 1 + grid_height * j],
-                        pt3: points[i + grid_height * (j + 1)],
+                    triangles.push(Triangle {
+                        pt1: Vertex {
+                            spatial: points[i + grid_height * j],
+                            uv: (),
+                        },
+                        pt2: Vertex {
+                            spatial: points[i + 1 + grid_height * j],
+                            uv: (),
+                        },
+                        pt3: Vertex {
+                            spatial: points[i + grid_height * (j + 1)],
+                            uv: (),
+                        },
                         color: get_color(),
-                    }));
-                    triangles.push(normal_form(Triangle {
-                        pt1: points[(i + 1) + grid_height * j],
-                        pt2: points[i + grid_height * (j + 1)],
-                        pt3: points[i + 1 + grid_height * (j + 1)],
+                    });
+                    triangles.push(Triangle {
+                        pt1: Vertex {
+                            spatial: points[(i + 1) + grid_height * j],
+                            uv: (),
+                        },
+                        pt2: Vertex {
+                            spatial: points[i + grid_height * (j + 1)],
+                            uv: (),
+                        },
+                        pt3: Vertex {
+                            spatial: points[i + 1 + grid_height * (j + 1)],
+                            uv: (),
+                        },
                         color: get_color(),
-                    }));
+                    });
                 }
             }
         }
@@ -111,58 +215,142 @@ fn make_triangle_partition(
     return triangles;
 }
 
-fn draw_triangle<P>(t: Triangle<Pt2, RGBA>, plotter: &mut P)
+fn simple_projection<T>(p: Vertex<Pt3, T>) -> Vertex<Pt3, T> {
+    let origin_pt = Pt3 {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+    };
+    let plane_unit_normal = Pt3 {
+        x: 0.0,
+        y: 0.0,
+        z: 1.0,
+    };
+    let t = (1.0 - p.spatial * plane_unit_normal) / ((p.spatial - origin_pt) * plane_unit_normal);
+    let projected_point = origin_pt * t + p.spatial * (1.0 - t);
+    return Vertex {
+        spatial: projected_point,
+        uv: p.uv,
+    };
+}
+fn spatial_to_pixel<T>(p: Vertex<Pt3, T>) -> Vertex<Pt2, T> {
+    let v = ViewportData {
+        plane_basis_x: Pt3 {
+            x: 1.0,
+            y: 0.0,
+            z: 0.0,
+        },
+        plane_basis_y: Pt3 {
+            x: 0.0,
+            y: 1.0,
+            z: 0.0,
+        },
+        x_min: -4.0,
+        x_max: 4.0,
+        y_min: -3.0,
+        y_max: 3.0,
+        pixels_tall: H,
+        pixels_wide: W,
+    };
+    let plane_x = p.spatial * v.plane_basis_x;
+    let plane_y = p.spatial * v.plane_basis_y;
+    if (plane_x < v.x_min) | (plane_x > v.x_max) | (plane_y < v.y_min) | (plane_y > v.y_max) {
+        return Vertex {
+            spatial: Pt2 {
+                x: 0,
+                y: 0,
+                clipped: true,
+            },
+            uv: p.uv,
+        };
+    };
+    let x = (v.pixels_wide as f64 * (plane_x - v.x_min) / (v.x_max - v.x_min)) as usize;
+    let y = (v.pixels_tall as f64 * (v.y_max - plane_y) / (v.y_max - v.y_min)) as usize;
+    return Vertex {
+        spatial: Pt2 {
+            x,
+            y,
+            clipped: false,
+        },
+        uv: p.uv,
+    };
+}
+
+fn prep_triangle<T>(t: Triangle<Vertex<Pt3, T>, RGBA>) -> Triangle<Vertex<Pt2, T>, RGBA>
 where
-    P: FnMut(Pt2, RGBA) -> (),
+    T: Copy,
 {
-    let y_start = t.pt1.y;
-    let y_end = std::cmp::max(t.pt2.y, t.pt3.y);
-    let y_mid = std::cmp::min(t.pt2.y, t.pt3.y);
-    let (left_m, left_b) = slope_intercept(t.pt1, t.pt2);
-    let (right_m, right_b) = slope_intercept(t.pt1, t.pt3);
-    let left_bottom = t.pt2.y <= t.pt3.y;
-    let (bottom_m, bottom_b) = slope_intercept(t.pt2, t.pt3);
+    return normal_form(Triangle {
+        pt1: spatial_to_pixel(simple_projection(t.pt1)),
+        pt2: spatial_to_pixel(simple_projection(t.pt2)),
+        pt3: spatial_to_pixel(simple_projection(t.pt3)),
+        color: t.color,
+    });
+}
+
+fn draw_triangle<P, Q>(t: Triangle<Vertex<Pt2, Q>, RGBA>, plotter: &mut P)
+where
+    P: FnMut(usize, usize, RGBA) -> (),
+{
+    let clipped = t.pt1.spatial.clipped | t.pt2.spatial.clipped | t.pt3.spatial.clipped;
+    if clipped {
+        return;
+    };
+    let y_start = t.pt1.spatial.y;
+    let y_end = std::cmp::max(t.pt2.spatial.y, t.pt3.spatial.y);
+    let y_mid = std::cmp::min(t.pt2.spatial.y, t.pt3.spatial.y);
+    let (left_m, left_b) = slope_intercept(t.pt1.spatial, t.pt2.spatial);
+    let (right_m, right_b) = slope_intercept(t.pt1.spatial, t.pt3.spatial);
+    let left_bottom = t.pt2.spatial.y <= t.pt3.spatial.y;
+    let (bottom_m, bottom_b) = slope_intercept(t.pt2.spatial, t.pt3.spatial);
     for y in y_start..y_end {
         let (x_left, x_right) = match y < y_mid {
             true => (
-                (y as f32 * left_m + left_b) as usize,
-                (y as f32 * right_m + right_b) as usize,
+                (y as f64 * left_m + left_b) as usize,
+                (y as f64 * right_m + right_b) as usize,
             ),
             false => match left_bottom {
                 true => (
-                    (y as f32 * bottom_m + bottom_b) as usize,
-                    (y as f32 * right_m + right_b) as usize,
+                    (y as f64 * bottom_m + bottom_b) as usize,
+                    (y as f64 * right_m + right_b) as usize,
                 ),
 
                 false => (
-                    (y as f32 * left_m + left_b) as usize,
-                    (y as f32 * bottom_m + bottom_b) as usize,
+                    (y as f64 * left_m + left_b) as usize,
+                    (y as f64 * bottom_m + bottom_b) as usize,
                 ),
             },
         };
         for x in x_left..x_right {
-            plotter(Pt2 { x, y }, t.color.clone());
+            plotter(x, y, t.color.clone());
         }
     }
 }
 
-fn slope_intercept(from: Pt2, to: Pt2) -> (f32, f32) {
-    let slope = (from.x as f32 - to.x as f32) / (from.y as f32 - to.y as f32);
-    let intercept = from.x as f32 - (from.y as f32 * slope);
+fn slope_intercept(from: Pt2, to: Pt2) -> (f64, f64) {
+    let slope = (from.x as f64 - to.x as f64) / (from.y as f64 - to.y as f64);
+    let intercept = from.x as f64 - (from.y as f64 * slope);
     return (slope, intercept);
 }
 
-fn normal_form(t: Triangle<Pt2, RGBA>) -> Triangle<Pt2, RGBA> {
+fn normal_form<T>(t: Triangle<Vertex<Pt2, T>, RGBA>) -> Triangle<Vertex<Pt2, T>, RGBA>
+where
+    T: Copy,
+{
     let mut pts = [t.pt1, t.pt2, t.pt3];
-    pts.sort_unstable_by(|a, b| (a.x + W * a.y).partial_cmp(&(b.x + W * b.y)).unwrap());
+    pts.sort_unstable_by(|a, b| {
+        (a.spatial.x + W * a.spatial.y)
+            .partial_cmp(&(b.spatial.x + W * b.spatial.y))
+            .unwrap()
+    });
     let pt1 = pts[0];
     let (v1_x, v1_y) = (
-        pts[0].x as i64 - pts[1].x as i64,
-        pts[0].y as i64 - pts[1].y as i64,
+        pts[0].spatial.x as i64 - pts[1].spatial.x as i64,
+        pts[0].spatial.y as i64 - pts[1].spatial.y as i64,
     );
     let (v2_x, v2_y) = (
-        pts[0].x as i64 - pts[2].x as i64,
-        pts[0].y as i64 - pts[2].y as i64,
+        pts[0].spatial.x as i64 - pts[2].spatial.x as i64,
+        pts[0].spatial.y as i64 - pts[2].spatial.y as i64,
     );
     let (pt2, pt3) = match v1_x * v2_y - v2_x * v1_y <= 0 {
         true => (pts[1], pts[2]),
@@ -200,8 +388,8 @@ fn main() -> Result<(), String> {
         .map_err(|e| e.to_string())?;
     // Create a red-green gradient
     let mut pixels = [0u8; 4 * W * H];
-    let mut plot = |pt: Pt2, color: RGBA| {
-        let idx = 4 * pt.x + 4 * W * pt.y;
+    let mut plot = |x: usize, y: usize, color: RGBA| {
+        let idx = 4 * x + 4 * W * y;
         let bytes = color.c.to_ne_bytes();
         if pixels[idx] as u32
             + pixels[idx + 1] as u32
@@ -209,52 +397,20 @@ fn main() -> Result<(), String> {
             + pixels[idx + 3] as u32
             != 0
         {
-            pixels[idx] = 0xFF;
-            pixels[idx + 1] = 0xFF;
-            pixels[idx + 2] = 0xFF;
-            pixels[idx + 3] = 0xFF;
+            pixels[idx..idx + 4].clone_from_slice(&[0xFF, 0xFF, 0xFF, 0xFF]);
             return;
         }
-        pixels[idx] = bytes[0];
-        pixels[idx + 1] = bytes[1];
-        pixels[idx + 2] = bytes[2];
-        pixels[idx + 3] = bytes[3];
+        pixels[idx..idx + 4].clone_from_slice(&bytes);
     };
 
-    let t1 = Triangle {
-        pt1: Pt2 { x: 5, y: 50 },
-        pt2: Pt2 { x: 50, y: 75 },
-        pt3: Pt2 { x: 10, y: 25 },
-        color: RGBA::new(0xFF00FF),
-    };
-    let t1 = normal_form(t1);
-    let t2 = Triangle {
-        pt1: Pt2 { x: 100, y: 55 },
-        pt2: Pt2 { x: 50, y: 75 },
-        pt3: Pt2 { x: 110, y: 15 },
-        color: RGBA::new(0xFFAAAA),
-    };
-    let t2 = normal_form(t2);
-    let tris = make_triangle_partition(320, 240, 10, 10, 10);
+    let tris = make_triangle_partition(-2.0, 2.0, -1.5, 2.5, 10, 10, 0.1);
     for tri in tris {
-        draw_triangle(tri, &mut plot);
+        draw_triangle(prep_triangle(tri), &mut plot);
     }
-    //draw_triangle(t1, &mut plot);
-    //draw_triangle(t2, &mut plot);
+
     texture
         .update(None, &pixels, 4 * W)
         .map_err(|e| e.to_string())?;
-
-    // texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
-    //     for y in 0..256 {
-    //         for x in 0..256 {
-    //             let offset = y * pitch + x * 3;
-    //             buffer[offset] = x as u8;
-    //             buffer[offset + 1] = y as u8;
-    //             buffer[offset + 2] = y as u8;
-    //         }
-    //     }
-    // })?;
 
     canvas.clear();
     canvas.copy(&texture, None, None)?;
@@ -270,7 +426,20 @@ fn main() -> Result<(), String> {
                     keycode: Some(Keycode::Escape),
                     ..
                 } => break 'running,
-                _ => {}
+                _ => {
+                    let tris = make_triangle_partition(-2.0, 2.0, -1.5, 2.5, 10, 10, 0.1);
+                    for tri in tris {
+                        draw_triangle(prep_triangle(tri), &mut plot);
+                    }
+
+                    texture
+                        .update(None, &pixels, 4 * W)
+                        .map_err(|e| e.to_string())?;
+
+                    canvas.clear();
+                    canvas.copy(&texture, None, None)?;
+                    canvas.present();
+                }
             }
         }
         // The rest of the game loop goes here...
